11948fbf0e438fa1d847026497b295ae842dca22
Andrew Rosen <rosen@cs.gsu.edu>
2014-04-16 21:28:09 -0400

    Alright, so I don't think it's findSuccessor's fault
    It can handle threads doing simultanious lookups

--- testChord.py
+++ testChord.py
@@ -1,5 +1,17 @@
 from pyChord import Node
 import time
+import random
+from threading import Thread
+from hashlib import sha1
+
+def randomLookups(nodes):
+    for i in range(100):
+        target = sha1(str(random.randint(0,1000000))).hexdigest()
+        print i
+        n = random.choice(nodes)
+        #print n.findSuccessor(target)
+    print "done"
+
 
 port = 9500
 
@@ -11,9 +23,21 @@ n2.join(n1.name)
 time.sleep(1)
 
 nodes = [n1,n2]
-for i in range(3,10):
+for i in range(3,5):
     time.sleep(0.5)
     n = Node("127.0.0.1",port+i)
     print "started", n
     nodes.append(n)
     n.join(n1.name)
+
+
+t1 = Thread(target = randomLookups, args = [nodes])
+t2 = Thread(target = randomLookups, args = [nodes])
+t3 = Thread(target = randomLookups, args = [nodes])
+t4 = Thread(target = randomLookups, args = [nodes])
+t5 = Thread(target = randomLookups, args = [nodes])
+t1.start()
+t2.start()
+t3.start()
+t4.start()
+t5.start()
