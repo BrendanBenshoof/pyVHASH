ae29f8bd4581555fed8253f2b2e7da7f2f8e3f13
Andrew Rosen <rosen@cs.gsu.edu>
2014-04-16 21:30:19 -0400

    Definately not findSuccessor's fault.

--- testChord.py
+++ testChord.py
@@ -5,11 +5,11 @@ from threading import Thread
 from hashlib import sha1
 
 def randomLookups(nodes):
-    for i in range(100):
+    for i in range(1000):
         target = sha1(str(random.randint(0,1000000))).hexdigest()
         print i
         n = random.choice(nodes)
-        #print n.findSuccessor(target)
+        n.findSuccessor(target)
     print "done"
 
 
