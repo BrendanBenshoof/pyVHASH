57cd17068ab08bf8ddf1da24739737a9b17a96f5
abrosen <rosen@cs.gsu.edu>
2014-04-24 15:31:11 -0400

    moved prev work to a new file

--- mapreduce.py
+++ mapreduce.py
@@ -5,12 +5,10 @@ from ChordDHT import DHTnode
 
 from threading import Thread, Lock
 
-class ChordReduceNode(DHTnode):
+class MapReduceNode(DHTnode):
     def __init__(self,host,ip):
         DHTnode.__init__(self,host,ip)
         self.addNewFunc(self.doMapReduce,"doMapReduce")
-        self.jobQueue = []
-        self.outQueue = []
 
     def mapFunc(self,key):
         print "mapfunc", key, self.name
@@ -39,3 +37,86 @@ class ChordReduceNode(DHTnode):
                 else:
                     data1[k]=data0[k]
         return data1 #overload this to describe reduce function
+
+    #private
+    def startMapReduce(self,filename):
+        key = getHashString(filename)
+        target = self.findSuccessor(key)
+        keyfile =  Peer(target).get(key) # why is this a dict?  rpc it turns out
+        keys = keyfile['keys'] 
+        return self.doMapReduce(keys)
+
+    #public
+    def doMapReduce(self,subKeys):
+        print  self.name, "doing Mapreduce"
+        buckets = self.buckitizeKeys(subKeys)
+        mytodo = []
+        print buckets
+
+        if self.name in buckets.keys():
+            mytodo = buckets[self.name]
+            del buckets[self.name]
+            print  self.name, "got my work"
+        threads = []
+        for othernode in buckets.keys():
+            threads.append(threadedMapReduceManager(othernode, buckets[othernode]))
+            print  self.name, "sending to ", othernode
+        for t in threads:
+            t.start()
+        #start work on my stuff while I wait for my peers
+        if len(mytodo) > 0:
+            print  self.name, "doing map"
+            results = map(self.mapFunc,mytodo)
+            if len(results) > 1:
+                result = reduce(self.reduceFunc, results)
+            else:
+                result = results[0]
+            print  self.name, "reduce done"
+        else:
+            result = False
+        for t in threads:
+            t.join()
+        for t in threads:
+            t.lock.acquire()
+            result = reduce(self.reduceFunc, [t.result, result])
+            t.lock.release()
+        return result
+
+
+        #report accumulated results
+
+    def buckitizeKeys(self,keylist):
+        output = {}
+        for k in keylist:
+            owner = None
+            if k in self.data.keys():  # the real question is why doesn't it work without this.
+                owner = self.name
+            else:
+                owner, t = self.find(k)
+            print owner
+            if owner in output.keys():
+                output[owner].append(k)
+            else:
+                output[owner] = [k]
+        return output
+
+
+class threadedMapReduceManager(Thread):
+    def __init__(self,remoteName, datapile):
+        Thread.__init__(self)
+        self.lock = Lock()
+        self.remoteName = remoteName
+        self.datapile = datapile
+        self.result = None
+
+    def run(self):
+        self.lock.acquire()
+        remote = Peer(self.remoteName)
+        print remote
+        print "start",self.name
+        self.result = remote.doMapReduce(self.datapile)
+        print "done",self.name
+        self.lock.release()
+        return True
+
+        
