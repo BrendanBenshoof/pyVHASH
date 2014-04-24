ccd7035c04e135910f0b54c4196be295b988bbe7
abrosen <rosen@cs.gsu.edu>
2014-04-24 17:11:19 -0400

    This is going to be complicated.

--- chordReduce.py
+++ chordReduce.py
@@ -1,15 +1,31 @@
 #chordReduce on ChordDHT
 from pyChord import Peer, getHashString
-
 from ChordDHT import DHTnode
-
+from cfs import DataAtom
 from threading import Thread, Lock
+import time
+
+
+
+class MapAtom(object):
+    jobid = None
+    hashid = None
+    stuffToMap = []
+    resultsAddress = 0
+
+
+class ReduceAtom(object):
+    jobid = None
+    results = []
+    keysInResults = []
+    resultsAddress = 0
 
 class ChordReduceNode(DHTnode):
     def __init__(self,host,ip):
         DHTnode.__init__(self,host,ip)
         self.addNewFunc(self.doMapReduce,"doMapReduce")
-        self.jobQueue = []
+        self.mapQueue = []
+        self.reduceQueue = []
         self.outQueue = []
 
     def mapFunc(self,key):
@@ -39,3 +55,16 @@ class ChordReduceNode(DHTnode):
                 else:
                     data1[k]=data0[k]
         return data1 #overload this to describe reduce function
+        
+    # first attempt
+    def doMap():
+        pass
+
+    def reduceLoop():
+        while True:
+            sleep(MAINT_INT*2)
+            while len(self.reduceQueue) >= 2:
+                atom1 = self.reduceQueue.pop()
+                atom2 = self.reduceQueue.pop()
+                self.reduceQueue.append(self.reduceFunc(atom1,atom2))
+            if len(self.reduceQueue)
--- scratch.py
+++ scratch.py
@@ -1,43 +1,10 @@
-from SimpleXMLRPCServer import SimpleXMLRPCServer
+class Test(object):
+    myVar = "a"
+    
+    def testMethod(self):
+        print myVar
+        
+        
+a = Test()
 
-class Server(object):
-    def __init__(self, hostport):
-        self.server = SimpleXMLRPCServer(hostport)
-
-    def register_function(self, function, name=None):
-        def _function(args, kwargs):
-            return function(*args, **kwargs)
-        _function.__name__ = function.__name__
-        self.server.register_function(_function, name)
-
-    def serve_forever(self):
-        self.server.serve_forever()
-
-#example usage
-
-class ServerProxy(object):
-    def __init__(self, url):
-        self._xmlrpc_server_proxy = xmlrpclib.ServerProxy(url)
-    def __getattr__(self, name):
-        call_proxy = getattr(self._xmlrpc_server_proxy, name)
-        def _call(*args, **kwargs):
-            return call_proxy(args, kwargs)
-        return _call
-#example usage
-
-
-server = Server(('localhost', 9000))
-def test(arg1, arg2):
-    print 'arg1: %s arg2: %s' % (arg1, arg2)
-    return 0
-server.register_function(test)
-server.serve_forever()
-
-
-
-serverp = ServerProxy('http://localhost:9000')
-serverp.test(1, 2)
-serverp.test(arg2=2, arg1=1)
-serverp.test(1, arg2=2)
-serverp.test(*[1,2])
-serverp.test(**{'arg1':1, 'arg2':2})
+a.testMethod()
