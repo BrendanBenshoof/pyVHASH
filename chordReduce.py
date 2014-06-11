2eabc13a74c66f2c8e993671effc4fd5396671ea
Andrew Rosen <rosen@cs.gsu.edu>
2014-06-11 19:12:24 -0400

    Organize before they rise!
    I'm organizing all the FT we need, then going to copy it over

--- chordReduce.py
+++ chordReduce.py
@@ -52,8 +52,54 @@ class ChordReduceNode(DHTnode):
 
 
 
-""" Mapreduce fault tolerence ACTIONS starts here """
+""" 
+Mapreduce fault tolerence ACTIONS starts here 
+Detecting what's gone wrong isn't the problem
+When things Go Wrong, this is how we ensure that things Go Right.
+I've split up the functions up into three categories:
+
+Results - The head honcho node collecting all the ReduceAtoms
+Maps -  Making sure the Map tasks get done
+Reduce - Making sure the Reduce tasks make it to the final destination
+
+I've further divided each part into two subparts:
+Normal Operations - How things SHOULD go, but won't because Murphy has a very sadistic sense of humor
+Churn Operations - How things are handled when Murphy decides to simulate an exploding computer or a new victim joining.
+
+An aside to the reader, all faults are considered equal.  It's a primitive solution, but I am just one man.
+"""
+### Results
+
+## Normal Operations 
+
+# backing up the results I just received.
+def backupNewResult():
+    pass
+
+# inform the owners of these keys that the reduceAtoms corresponding to the hashkeys were received
+
+
+## Churn operations
+# My predecessor has died.  He had the results.  Bugger.
+
+# I'm holding the results and decided that my predecessor is a better candidate to hold this ticking time bomb we call "results" than me.
 
 
 
-""" End Mapreduce Fault tolerence actions """ 
+### Maps
+## Normal Operations 
+#Send backup of map task to successor list 
+
+#Notify sucessors of completion of map task
+
+
+
+
+### Reduce
+
+## Normal Actions
+# Backup the reduce atom I just created.  Also let them know that if I die, I want them to avenge me.  Or at the very least, ensure the reduce atom makes its way to the 
+# Notify successors that the reduce atom successfully made it's way to the results 
+"""
+End Mapreduce Fault tolerence actions 
+""" 
