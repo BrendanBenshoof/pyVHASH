67758da88fa1d20181b7e3b181b2464a09e31d09
Andrew Rosen <rosen@cs.gsu.edu>
2014-07-10 19:51:07 -0400

    Working on lookup; approx region should be done
    Music:  Bad Apple
    
    https://www.youtube.com/watch?v=xr3VqNlS6KE

--- New folder/vhash_routing_test.py
+++ New folder/vhash_routing_test.py
@@ -2,6 +2,7 @@ from vhash_greedy import *
 import random
 
 random.seed(12345)
+TABLE_SIZE = 3*d +1
 
 def simulate_routing(nodes):
     pass
@@ -24,14 +25,34 @@ class Node(object):
 
         
     def update_peers(self,candidates):
-        pass
+        self.peers = self.approx_region()
         
     def approx_region(self, candidates):
         new_peers = []
         candidates =  sorted(candidates, key = lambda x: dist(self.loc, x.loc)) #sort candidates
-        new_peers =  candidates.pop(0)
+        new_peers.append(candidates.pop(0))
         for c in candidates[:]:
-            midpoint = calc_midpoint(self.loc,c)
+            midpoint = calc_midpoint(self.loc,c)  # find the midpoint between myself and the new point
+            dist_to_midpoint = dist(self.loc, midpoint)
+            good = True
+            for p in new_peers:  # now for each peer that I've added
+                if dist(p.loc, midpoint) < dist_to_midpoint: # if p is closer to the midpoint than I am...
+                    good = False
+                    break # reject it
+            if good:
+                candidates.remove(c)
+                new_peers.append(c)
+        while len(new_peers) < TABLE_SIZE and len(candidates) > 0:
+            new_peers.append(candidates.pop(0))
+        return new_peers
+
+    def lookup(self, loc):
+        if len(self.peers) == 0:
+            return self
+        best_peer =  min(self.peers, key = lambda x: dist(loc, x.loc))
+        
+
+
             
 
 
