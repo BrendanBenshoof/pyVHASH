cb7b03aee02105cc87d876aa3e137f05661d1f06
abrosen <rosen@cs.gsu.edu>
2014-07-11 14:26:22 -0400

    WOOOOOOOO

--- New folder/vhash_routing_test.py
+++ New folder/vhash_routing_test.py
@@ -3,12 +3,12 @@ import random
 
 random.seed(12345)
 TABLE_SIZE = 3*d +1
-NETWORK_SIZE = 1000
-CYCLES = 1000
+NETWORK_SIZE = 500
+CYCLES = 100
 
 def simulate_routing(nodes):
     correct = 0.0
-    samples = 1000
+    samples = 100
     for i in range(0,samples):
         p = randPoint()
         start = random.choice(nodes)
@@ -30,22 +30,30 @@ class Node(object):
         yenta = random.choice(self.peers) # yenta is yiddish for a rumormonger
         
         #need to remove self and yenta from our own lists
-        my_candidates = self.peers + list(set(yenta.peers) - set(self.peers))  # In set notaion A <- A union B
-        yenta_candidates = yenta.peers + list(set(self.peers) - set(yenta.peers))
+        my_candidates = self.create_candidates(yenta)  # In set notaion A <- A union B
+        yenta_candidates = yenta.create_candidates(self)
         
         self.update_peers(my_candidates)
         yenta.update_peers(yenta_candidates)
 
+    def create_candidates(self, yenta):
+        candidates = self.peers
+        rumors = [yenta] + yenta.peers 
+        for r in rumors:
+            if r not in candidates and not(r is self):  # possibe error point
+                candidates.append(r)
+        return candidates
+
         
     def update_peers(self,candidates):
-        self.peers = self.approx_region()
+        self.peers = self.approx_region(candidates)
         
     def approx_region(self, candidates):
         new_peers = []
-        candidates =  sorted(candidates, key = lambda x: dist(self.loc, x.loc)) #sort candidates
+        candidates =  sorted(candidates, key=lambda x: dist(self.loc, x.loc)) #sort candidates
         new_peers.append(candidates.pop(0))
         for c in candidates[:]:
-            midpoint = calc_midpoint(self.loc,c)  # find the midpoint between myself and the new point
+            midpoint = calc_midpoint(self.loc,c.loc)  # find the midpoint between myself and the new point
             dist_to_midpoint = dist(self.loc, midpoint)
             good = True
             for p in new_peers:  # now for each peer that I've added
@@ -73,10 +81,25 @@ class Node(object):
         parent = member.lookup(self.loc)
         self.peers.append(parent)
         self.gossip()
+        #or gossip with all peers
 
 # Goals print out routing success rate, average degree, largest degree
 
 if __name__ ==  '__main__':
     nodes = []
-    for i in range(0,NETWORK_SIZE):
+    bootstrapper = Node()
+    nodes.append(bootstrapper) 
+    for i in range(1,NETWORK_SIZE):
         n = Node()
+        parent = random.choice(nodes)
+        n.join(parent)
+        nodes.append(n)
+        
+
+for i in range(0,CYCLES):
+    for node in nodes:
+        node.gossip()
+    print simulate_routing(nodes)
+
+for node in nodes:
+    print node.loc, [x.loc for x in node.peers]
