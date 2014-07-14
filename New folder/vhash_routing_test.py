b4e1ce87257bb311f2b2dbd067ae147470ff576c
Andrew Rosen <rosen@cs.gsu.edu>
2014-07-14 15:17:07 -0400

    verygood.wav
    DONE ADDING
    0 0.97 8 20.29 36
    1 0.99 8 20.5 36
    2 0.99 8 20.71 36
    3 1.0 8 20.93 36
    4 1.0 8 21.01 36
    5 0.99 8 21.09 36
    6 1.0 8 21.12 36
    7 0.99 8 21.16 36
    8 1.0 8 21.17 36
    9 1.0 8 21.17 36
    10 1.0 8 21.18 36
    11 1.0 8 21.18 36
    12 1.0 8 21.2 36
    13 1.0 8 21.2 36
    14 1.0 8 21.21 36
    15 1.0 8 21.21 36
    16 1.0 8 21.22 36
    17 1.0 8 21.24 36
    18 1.0 8 21.24 36
    19 1.0 8 21.24 36
    20 1.0 8 21.24 36
    21 1.0 8 21.24 36
    22 1.0 8 21.24 36
    etc

--- New folder/vhash_routing_test.py
+++ New folder/vhash_routing_test.py
@@ -80,8 +80,8 @@ class Node(object):
             if good:
                 candidates.remove(c)
                 new_peers.append(c)
-        while len(new_peers) < TABLE_SIZE and len(candidates) > 0:  #is this block nessecary
-            new_peers.append(candidates.pop(0))
+        #while len(new_peers) < TABLE_SIZE and len(candidates) > 0:  #is this block nessecary
+        #    new_peers.append(candidates.pop(0))
         return new_peers
 
     def lookup(self, loc):
