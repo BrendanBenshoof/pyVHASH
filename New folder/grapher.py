fd7bd7e9c32e917511edd00a70d88d1b3dbdac35
Andrew Rosen <rosen@cs.gsu.edu>
2014-07-21 19:56:27 -0400

    results

--- New folder/grapher.py
+++ New folder/grapher.py
@@ -1,7 +1,7 @@
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
-d=5
+d=3
 
 def get_accuracy(filename):
     accuracy = []
@@ -13,14 +13,14 @@ def get_accuracy(filename):
 
 
 plt.xkcd()
-plt.plot(np.arange(0,100),get_accuracy('n_500_d_'+str(d)+'.csv'), label='n=500', marker='o', linestyle= '-', color='b')
-plt.plot(np.arange(0,100),get_accuracy('n_1000_d_'+str(d)+'.csv'), label='n=1000', marker='.', linestyle= '-', color='r')
-plt.plot(np.arange(0,100),get_accuracy('n_2000_d_'+str(d)+'.csv'), label='n=2000', marker='*', linestyle= '-', color='g')
-plt.plot(np.arange(0,100),get_accuracy('n_5000_d_'+str(d)+'.csv'), label='n=5000', marker='x', linestyle= '-', color='k')
-#plt.plot(np.arange(0,100),get_accuracy('n_10000_d_'+str(d)+'.csv'), label='n=10000', marker='p', linestyle= '-', color='y')
+plt.plot(np.arange(0,100),get_accuracy('n_500_d_'+str(d)+'.csv'), label='n=500', marker='o', linestyle= '-.', color='k')
+plt.plot(np.arange(0,100),get_accuracy('n_1000_d_'+str(d)+'.csv'), label='n=1000', marker='.', linestyle= '-', color='k')
+plt.plot(np.arange(0,100),get_accuracy('n_2000_d_'+str(d)+'.csv'), label='n=2000', marker='*', linestyle= '--', color='k')
+plt.plot(np.arange(0,100),get_accuracy('n_5000_d_'+str(d)+'.csv'), label='n=5000', marker='x', linestyle= '--', color='k')
+#plt.plot(np.arange(0,100),get_accuracy('n_10000_d_'+str(d)+'.csv'), label='n=10000', marker='p', linestyle= ':', color='k')
 
-plt.xlabel('Cycle')
-plt.ylabel('Accuracy')
-plt.title('Routing Convergence in d=' +str(d))
+plt.xlabel('Level of familiarity with the docs')
+plt.ylabel('Ease of coding')
+plt.title('Apparently, matplotlib has xkcd mode')
 plt.legend()
 plt.show()
