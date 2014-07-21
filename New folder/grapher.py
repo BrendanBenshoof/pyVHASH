97a448e7f13f678e996c361cac5e46ec3a38506e
Andrew Rosen <rosen@cs.gsu.edu>
2014-07-21 18:49:34 -0400

    This will output graphs for the routing data.

--- New folder/grapher.py
+++ New folder/grapher.py
@@ -1,10 +1,26 @@
 import csv
+import matplotlib.pyplot as plt
+import numpy as np
+d=5
 
-
-def get_data(filename):
+def get_accuracy(filename):
+    accuracy = []
     with open(filename, 'rb') as csvfile:
         datareader =csv.reader(csvfile, delimiter=',')
         for row in datareader:
-            print row
+            accuracy.append(row[1])
+    return accuracy
 
-get_data('n_1000_d_2.csv') 
+
+plt.xkcd()
+plt.plot(np.arange(0,100),get_accuracy('n_500_d_'+str(d)+'.csv'), label='n=500', marker='o', linestyle= '-', color='b')
+plt.plot(np.arange(0,100),get_accuracy('n_1000_d_'+str(d)+'.csv'), label='n=1000', marker='.', linestyle= '-', color='r')
+plt.plot(np.arange(0,100),get_accuracy('n_2000_d_'+str(d)+'.csv'), label='n=2000', marker='*', linestyle= '-', color='g')
+plt.plot(np.arange(0,100),get_accuracy('n_5000_d_'+str(d)+'.csv'), label='n=5000', marker='x', linestyle= '-', color='k')
+#plt.plot(np.arange(0,100),get_accuracy('n_10000_d_'+str(d)+'.csv'), label='n=10000', marker='p', linestyle= '-', color='y')
+
+plt.xlabel('Cycle')
+plt.ylabel('Accuracy')
+plt.title('Routing Convergence in d=' +str(d))
+plt.legend()
+plt.show()
