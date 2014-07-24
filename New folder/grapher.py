b73de467c33f2a13efc9a5c1e9ac9ff1ff12383a
abrosen <rosen@cs.gsu.edu>
2014-07-24 13:07:44 -0400

    grapher changes

--- New folder/grapher.py
+++ New folder/grapher.py
@@ -12,15 +12,15 @@ def get_accuracy(filename):
     return accuracy
 
 
-plt.xkcd()
-plt.plot(np.arange(0,100),get_accuracy('n_500_d_'+str(d)+'.csv'), label='n=500', marker='o', linestyle= '-.', color='k')
-plt.plot(np.arange(0,100),get_accuracy('n_1000_d_'+str(d)+'.csv'), label='n=1000', marker='.', linestyle= '-', color='k')
-plt.plot(np.arange(0,100),get_accuracy('n_2000_d_'+str(d)+'.csv'), label='n=2000', marker='*', linestyle= '--', color='k')
-plt.plot(np.arange(0,100),get_accuracy('n_5000_d_'+str(d)+'.csv'), label='n=5000', marker='x', linestyle= '--', color='k')
+#plt.xkcd()
+plt.plot(np.arange(0,100),get_accuracy('n_500_d_'+str(d)+'.csv'), label='n=500', marker='o', linestyle= '-.')
+plt.plot(np.arange(0,100),get_accuracy('n_1000_d_'+str(d)+'.csv'), label='n=1000', marker='.', linestyle= '-')
+plt.plot(np.arange(0,100),get_accuracy('n_2000_d_'+str(d)+'.csv'), label='n=2000', marker='*', linestyle= '--')
+plt.plot(np.arange(0,100),get_accuracy('n_5000_d_'+str(d)+'.csv'), label='n=5000', marker='x', linestyle= '--')
 #plt.plot(np.arange(0,100),get_accuracy('n_10000_d_'+str(d)+'.csv'), label='n=10000', marker='p', linestyle= ':', color='k')
 
-plt.xlabel('Level of familiarity with the docs')
-plt.ylabel('Ease of coding')
-plt.title('Apparently, matplotlib has xkcd mode')
+plt.xlabel('Cycles')
+plt.ylabel('Accuracy')
+plt.title('d=' + str(d))
 plt.legend()
 plt.show()
